1e4c9b15bd1abf1340afff9d7cd0227b2cd9fc79
Errant404 <erigidissimus@gmail.com>
2024-09-12 15:41:01 +0800

    Enable multiprocessing

--- src/rlmode-deap.py
+++ src/rlmode-deap.py
@@ -1,10 +1,14 @@
 import random
-from math import factorial
+import multiprocessing
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pymop.factory
+from math import factorial
 from deap import base, creator, tools
+
+# Enable multiprocessing
+pool = multiprocessing.Pool() 
 
 # Problem definition
 PROBLEM = "dtlz2"
@@ -45,7 +49,7 @@ toolbox = base.Toolbox()
 toolbox.register("attr_float", uniform, BOUND_LOW, BOUND_UP, NDIM)
 toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.attr_float)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-
+toolbox.register("map", pool.map)
 toolbox.register("evaluate", problem.evaluate, return_values_of=["F"])
 toolbox.register("mate", tools.cxSimulatedBinaryBounded, low=BOUND_LOW, up=BOUND_UP, eta=30.0)
 toolbox.register("mutate", tools.mutPolynomialBounded, low=BOUND_LOW, up=BOUND_UP, eta=20.0, indpb=1.0 / NDIM)
@@ -188,10 +192,12 @@ if __name__ == "__main__":
     plt.legend()
     plt.title("RLMODE Results on DTLZ2")
     plt.tight_layout()
-    plt.savefig("rlmode_dtlz2.png")
     plt.show()
 
     # Calculate and print IGD
     from deap.benchmarks.tools import igd
 
     print(f"IGD: {igd(pop_fit, pf)}")
+
+pool.close()
+pool.join()
